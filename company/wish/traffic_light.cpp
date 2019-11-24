144839b2a63b876afb09f7f71d5f292cf62a970c
Haoyang Chen <chenhaoy@usc.edu>
2019-11-24 16:21:44 -0500

    udpdate sol for traffic light

--- company/wish/traffic_light.cpp
+++ company/wish/traffic_light.cpp
@@ -1,36 +1,27 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-  int test[] = {1,1,1,1,0,1,0,1,1,0,0,0,}; // red: 1, green: 0
-  // find the longest range where # of 1 - # of 0 is the
-  int prevMax = test [0];
-  int start = 0, end = 0, count = 0;
-  for (int i = 1; i < (int) (sizeof (test) / sizeof (int)); ++i)
-   {
-      int currMax = 0;
-      if (test [i] == 0) // green
-        {
-          currMax = max (prevMax - 1, -1);
-          if (currMax == -1)
-            start = i;
-          prevMax = currMax;
-        }
-      else // red
-        {
-          currMax = max (prevMax + 1, 1);
-          if (currMax == 1)
-            start = i;
-          prevMax = currMax;
-        }
-      if (currMax > count)
-        {
-          count = currMax;
-          end = i;
+    // int test[] = {1,1,1,1,0,1,0,1,1,0,0,0,}; // red: 1, green: -1
+    int test[] = { -1, 1, 1, -1, 1, 1 }; // 1 , 2
+    // find the longest range where # of 1 - # of 0
+    // in the range the smaller the # of 0s the better
+    int prevMax = test[0];
+    int currStart = 0, end = 0, count = 0;
+    int start = 0;
+    for (int i = 1; i < (int)(sizeof(test) / sizeof(int)); ++i) {
+        int currMax = max(test[i], prevMax + test[i]);
+        if (currMax == test[i])
+            currStart = i;
+        prevMax = currMax;
+        if (currMax > count) {
+            count = currMax;
+            start = currStart;
+            end = i;
         }
     }
-  printf ("start: %d, end: %d\n", start, end);
-  return 0;
+    printf("start: %d, end: %d\n", start, end);
+    return 0;
 }
